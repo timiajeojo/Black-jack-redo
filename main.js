f87f1a6f11964ded270dc702384b362b95944ccf
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-12 15:21:28 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let  player = {
+  name: "Timi",
+  chips: 120
+}
+let sum = 0
+let cards = []
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+ let messageEl = document.getElementById("message-el")
+ let sumEl = document.getElementById("sum-el")
+ let  cardsEl = document.getElementById("cards-el")
+ let playerEl = document.getElementById("player-el")
