2dbbcf15791ec7d24a9c197bd02b5ac034900da6
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-16 08:26:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ let message = ""
  let sumEl = document.getElementById("sum-el")
  let  cardsEl = document.getElementById("cards-el")
  let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
+ 
+ function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+
+    if (randomNumber === 1) {
+        return 11;
+    } else if (randomNumber > 10) {
+        return 10;
+    } else {
+        return randomNumber;
+    }
+}
