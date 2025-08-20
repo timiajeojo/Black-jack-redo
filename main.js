1e784516915bd0c0179553a765d58e24d21333f9
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-19 20:12:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,25 @@ playerEl.textContent = player.name + ": $" + player.chips;
         return randomNumber;
     }
 }
+
+
+function startGame() {
+   isAlive = truepl
+   let firstCard = getRandomCard();
+   let secondCard = getRandomCard();
+   let card = [firstCard, secondCard]
+   let sum = firstCard + secondCard;
+   renderGame()
+ }
+ 
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " 
+  
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum
+  
+if (sum <= 20) {
+ message = "Do you want to draw new card?" }
